2207bc96302c53752cbbd457d707044cd788b030
Davidson Schaly <davidsonturbo@gmail.com>
2022-02-17 01:23:52 -0300

    Implementing Paged Search in Book Controller

--- 02_RestWithASPNET_Person/RestWithASPNET/RestWithASPNET/Controllers/BookController.cs
+++ 02_RestWithASPNET_Person/RestWithASPNET/RestWithASPNET/Controllers/BookController.cs
@@ -31,15 +31,19 @@ namespace RestWithASPNET.Controllers
 
         // Maps GET requests to https://localhost:{port}/api/book
         // Get no parameters for FindAll -> Search All
-        [HttpGet]
+        [HttpGet("{sortDirection}/{pageSize}/{page}")]
         [TypeFilter(typeof(HyperMediaFilter))]
         [ProducesResponseType((200), Type = typeof(List<BookDTO>))]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
         [ProducesResponseType(401)]
-        public IActionResult Get()
+        public IActionResult Get(
+            [FromQuery] string book,
+            string sortDirection,
+            int pageSize,
+            int page)
         {
-            return Ok(_bookBusiness.FindAll());
+            return Ok(_bookBusiness.SearchPaged(book, sortDirection, pageSize, page));
         }
 
         // Maps GET requests to https://localhost:{port}/api/book/{id}
